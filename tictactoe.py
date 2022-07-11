936c1af5515ec7c70eed4d02d62ce73e30bf2f68
Sergey Bastrikov <sergeywhitedev@gmail.com>
2022-07-11 09:57:58 +0500

    stage 2

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,7 @@
 # write your code here
+S = input().strip()
+print("---------")
+print("|", S[0], S[1], S[2], "|")
+print("|", S[3], S[4], S[5], "|")
+print("|", S[6], S[7], S[8], "|")
+print("---------")
