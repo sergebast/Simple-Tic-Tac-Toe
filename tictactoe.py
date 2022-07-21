e29ba2f19129bc8c9763c618ab7c2428a53a3220
Sergey Bastrikov <sergeywhitedev@gmail.com>
2022-07-22 00:26:47 +0500

    Stage 5/5: final

--- tictactoe.py
+++ tictactoe.py
@@ -1,36 +1,71 @@
-from checkgame import matrix
+m = n = 3
+step = 1
+my_matrix = [['_'] * m for i in range(n)]
 
 
-def game_table(m):
+def game_table(_matrix):
     print("---------")
-    for x in m:
+    for x in _matrix:
         print("|", " ".join(x), "|")
     print("---------")
 
 
-s = list(input().strip())
-my_matrix = matrix(s)
-print("My_matrix", my_matrix)
-game_table(my_matrix)
+def get_cols(_matrix):
+    r = []
+    for i in range(n):
+        r.append([x[i] for x in _matrix])
+    return r
 
-while True:
-    try:
-        i = input().split()
-        a = int(i[0]) - 1
-        b = int(i[1]) - 1
-    except ValueError:
-        print("You should enter numbers!")
+
+def get_diagonal(_matrix):
+    return [[_matrix[0][0], _matrix[1][1], _matrix[2][2]],
+            [_matrix[0][2], _matrix[1][1], _matrix[2][0]]]
+
+
+def combinations(_matrix):
+    return get_cols(_matrix) \
+           + get_diagonal(_matrix) \
+           + _matrix
+
+
+def user(_step):
+    if _step % 2 == 0:
+        return 'O'
     else:
-        if 0 <= a < 3:
-            if 0 <= b < 3:
+        return 'X'
+
+
+def counter_element(el, _matrix):
+    return len([x for row in _matrix for x in row if x == el])
+
+
+if __name__ == '__main__':
+
+    game_table(my_matrix)
+
+    while True:
+        try:
+            i = input().split()
+            a = int(i[0]) - 1
+            b = int(i[1]) - 1
+        except ValueError:
+            print("You should enter numbers!")
+        except IndexError:
+            print("Coordinates should be from 1 to 3!")
+        else:
+            if all([a >= 0, a < 3, b >= 0, b < 3]):
                 if my_matrix[a][b] == " " or my_matrix[a][b] == "_":
-                    my_matrix[a][b] = 'X'
-                    break
+                    my_matrix[a][b] = user(step)
+                    game_table(my_matrix)
+                    data = combinations(my_matrix)
+                    if any([['X', 'X', 'X'] in data, ['O', 'O', 'O'] in data]):
+                        print(user(step), 'wins')
+                        break
+                    elif counter_element('_', my_matrix) == 0:
+                        print('Draw')
+                        break
+                    step += 1
                 else:
                     print("This cell is occupied! Choose another one!")
             else:
                 print("Coordinates should be from 1 to 3!")
-        else:
-            print("Coordinates should be from 1 to 3!")
-
-game_table(my_matrix)
